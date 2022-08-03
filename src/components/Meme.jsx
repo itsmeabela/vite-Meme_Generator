67b7fd44eae5d259b9277c2ca01020a4b884edc5
abelrobel7@gmail.com <abelrobel7@gmail.com>
2022-08-03 12:29:42 +0300

    text form works

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -8,14 +8,24 @@ export default function Meme() {
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
-    randomImage: "https://i.imgflip.com/30b1gx.jpg",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
   });
+
+  function saveText(e) {
+    const { name, value } = e.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+  console.log(meme);
+
   const [allMemeImages, setallMemesImages] = useState(MemeData);
 
   function getimg() {
     const memesArr = MemeData.data.memes;
     let randomNum = Math.floor(Math.random() * memesArr.length);
-    const url = memesArr[randomNum];
+    const { url } = memesArr[randomNum];
     setMeme((prev) => ({
       ...prev,
       randomImage: url,
@@ -25,13 +35,31 @@ export default function Meme() {
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          onChange={saveText}
+          value={meme.topText}
+          name="topText"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          onChange={saveText}
+          value={meme.bottomText}
+          name="bottomText"
+        />
         <button className="form--button" onClick={getimg}>
           Get a new meme image 🎭
         </button>
       </div>
-      <img src={meme.randomImage} className="meme-img" alt="" />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
